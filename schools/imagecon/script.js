c824c8026ad00a9ce69818977bd08585574ae2d7
NongZen <122343544+NongZen@users.noreply.github.com>
2023-07-29 22:54:58 +0700

    Update script.js

--- schools/imagecon/script.js
+++ schools/imagecon/script.js
@@ -6,44 +6,85 @@ function setOutputType(type) {
   outputButton.textContent = `Output File Type: ${type.toUpperCase()}`;
 }
 
-function convertAndDownload() {
+async function processZipFile(file) {
+  const zip = await JSZip.loadAsync(file);
+
+  const imageFiles = Object.values(zip.files).filter((file) => {
+    const extension = file.name.split('.').pop().toLowerCase();
+    return ['jpg', 'jpeg', 'png', 'webp', 'avif'].includes(extension);
+  });
+
+  if (imageFiles.length === 0) {
+    alert('The ZIP file does not contain any supported image files (jpg, jpeg, png, webp, avif).');
+    return;
+  }
+
+  const imageFilePromises = imageFiles.map(async (imageFile) => {
+    const dataURL = await imageFile.async('base64');
+    const extension = outputType === "jpg" ? "jpeg" : outputType;
+    return {
+      name: `${imageFile.name.split(".")[0]}.${extension}`,
+      dataURL: dataURL.split(",")[1]
+    };
+  });
+
+  return await Promise.all(imageFilePromises);
+}
+
+async function convertAndDownload() {
   const files = document.getElementById("fileInput").files;
   const progressBar = document.getElementById("progressBar");
   const downloadLink = document.getElementById("downloadLink");
 
   if (files.length === 0) {
-    alert("Please select one or more images.");
+    alert("Please select one or more images or a ZIP file.");
     return;
   }
 
   progressBar.style.display = "block";
   downloadLink.style.display = "none";
+  document.getElementById("adds").innerHTML = "";
 
-  if (files.length === 1) {
-    // If there's only one image, directly download without zipping
-    document.getElementById("adds").innerHTML = ""
-    const file = files[0];
-    const reader = new FileReader();
-    reader.onloadend = function () {
-      const dataURL = reader.result;
-      const link = document.createElement("a");
-      downloadLink.href = dataURL;
-      downloadLink.download = `${file.name.split(".")[0]}_${outputType.toUpperCase()}`;;
-      downloadLink.style.display = "block";
+  if (files.length === 1 && files[0].name.endsWith('.zip')) {
+    // If there's only one zip file, process it separately
+    try {
+      const imageFiles = await processZipFile(files[0]);
+      if (imageFiles && imageFiles.length > 0) {
+        // Create a zip for the converted images
+        const zip = new JSZip();
+        imageFiles.forEach((imageFile) => {
+          zip.file(imageFile.name, imageFile.dataURL, { base64: true });
+        });
+
+        zip.generateAsync({ type: "blob" })
+          .then(function (content) {
+            const url = URL.createObjectURL(content);
+            downloadLink.href = url;
+            downloadLink.download = `converted_images.zip`;
+            downloadLink.style.display = "block";
+            progressBar.style.display = "none";
+          });
+      }
+    } catch (error) {
+      alert('Error processing the ZIP file.');
       progressBar.style.display = "none";
-    };
-    reader.readAsDataURL(file);
+    }
   } else {
-    // If there are multiple images, create a zip file
-    document.getElementById("adds").innerHTML = "s"
+    // Handle conversion for individual image files
     const zip = new JSZip();
 
     let processedCount = 0;
 
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
+      if (file.name.endsWith('.zip')) {
+        // Skip zip files, we already handled them above
+        processedCount++;
+        continue;
+      }
+
       const reader = new FileReader();
-      reader.onloadend = function () {
+      reader.onloadend = async function () {
         const dataURL = reader.result;
         const extension = outputType === "jpg" ? "jpeg" : outputType; // jpg should be converted to jpeg
         zip.file(`${file.name.split(".")[0]}.${extension}`, dataURL.split(",")[1], { base64: true });
@@ -54,7 +95,7 @@ function convertAndDownload() {
             .then(function (content) {
               const url = URL.createObjectURL(content);
               downloadLink.href = url;
-              downloadLink.download = `converted_image.zip`;
+              downloadLink.download = `converted_images.zip`;
               downloadLink.style.display = "block";
               progressBar.style.display = "none";
             });
